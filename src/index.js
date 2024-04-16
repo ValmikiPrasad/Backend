24c03168a3b7c76949bfeb699fbb8f6551463800
ValmikiPrasad <1valmikigupta@gmail.com>
2024-04-16 23:05:58 +0530

    project

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config()
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({path:'./env'})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+// function connectDB(){
+
+// }
+// connectDB()
+
+
+
+/*
+const app=express();
+(async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERR : ",error)
+            throw error
+        });
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`server is running at ${process.env.PORT}`)
+        })
+    }
+    catch(err){
+        console.log(err)
+        throw err
+    }
+})()*/
